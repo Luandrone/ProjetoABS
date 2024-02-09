b044e971b5eda8fa94f0d64ce65ec9ebb88b7d2d
Luan Souza <luanetec@yahoo.com.br>
2024-02-09 11:34:34 -0300

    alteração na função formaDeEntrega

--- projetoAbs.js
+++ projetoAbs.js
@@ -1,17 +1,18 @@
 const readline = require('readline');
 
 const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
+    input: process.stdin,
+    output: process.stdout
 });
 
-class Bebida{
-    constructor(bebida, copo, gelo, tamanho, entrega){
+class Bebida {
+    constructor(bebida, copo, gelo, tamanho, entrega, tampa) {
         this.bebida = bebida;
         this.copo = copo;
         this.gelo = gelo;
         this.tamanho = tamanho;
         this.entrega = entrega;
+        this.entrega = tampa;
     }
 
 }
@@ -21,35 +22,34 @@ let bebidaEscolhida = new Bebida
 escolherBebidas()
 
 function escolherBebidas() {
-    rl.question('Escolha sua bebida, Refrigerantes: Coca, Guaraná, Suco de Uva ou Suco de Laranja: ', (bebida) => {
-            
-        if (bebida === 'Coca' || bebida === 'Guaraná'){
 
-            console.log(`Sua bebida Refrigerante ${bebida} será servida no copo de papel.`)  
-            bebidaEscolhida.bebida = bebida
-            bebidaEscolhida.copo = 'papel'
+    pergunta('Escolha sua bebida, Refrigerantes: Coca, Guaraná, Suco de Uva ou Suco de Laranja: ', (bebida) => {
+    
+            if (bebida === 'Coca' || bebida === 'Guaraná') {
 
-            pedrasDeGelo(6)
-            
+                console.log(`Sua bebida Refrigerante ${bebida} será servida no copo de papel.`)
+                bebidaEscolhida.bebida = bebida
+                bebidaEscolhida.copo = 'papel'
+                pedrasDeGelo(6)
 
-        } else {
+            } else {
 
-            console.log(`Sua bebida Suco de ${bebida} será servida no copo de plástico.`)
-            bebidaEscolhida.bebida = bebida
-            bebidaEscolhida.copo = 'plástico'
+                console.log(`Sua bebida Suco de ${bebida} será servida no copo de plástico.`)
+                bebidaEscolhida.bebida = bebida
+                bebidaEscolhida.copo = 'plástico'
 
-            pedrasDeGelo(12)
-            
-        }
-          
-    });
+                pedrasDeGelo(12)
+
+            }
+    })
+
 }
 
 function pedrasDeGelo(pedrasDeGelo) {
-    rl.question('Você gostaria de pedras de gelo, Sim ou Não ? ', (resposta) =>{
-        if(resposta === 'Sim') {
+    pergunta('Você gostaria de pedras de gelo, Sim ou Não ? ', (resposta) => {
+        if (resposta === 'Sim') {
             console.log(`Você recebeu ${pedrasDeGelo} pedras de gelo`)
-            bebidaEscolhida.gelo = pedrasDeGelo 
+            bebidaEscolhida.gelo = pedrasDeGelo
         } else {
             console.log('Você optou por beber sem gelo')
             bebidaEscolhida.gelo = 0
@@ -59,22 +59,33 @@ function pedrasDeGelo(pedrasDeGelo) {
     })
 }
 
-function tamanhoDoCopo(){
-    rl.question('Escolha o tamanho do copo, 300ml, 500ml, 700ml: ', (tamanhoml) => {
-        console.log(`Seu tamanho foi de ${tamanhoml}`)
-        bebidaEscolhida.tamanho = tamanhoml
+function tamanhoDoCopo() {
+    pergunta('Escolha o tamanho do copo, 300ml, 500ml, 700ml: ', (tamanhoMl) => {
+        console.log(`Seu tamanho foi de ${tamanhoMl}`)
+        bebidaEscolhida.tamanho = tamanhoMl
 
-        formaDeEntrega() 
+        formaDeEntrega()
     })
 }
 
-function formaDeEntrega(){
-    rl.question('Escolha sua forma de entrega, delivery ou comer no local: ', (entrega) => {
+function formaDeEntrega() {
+    pergunta('Escolha sua forma de entrega, delivery ou comer no local: ', (entrega) => {
+
+        if(entrega === 'delivery') {
+            bebidaEscolhida.tampa = 'tampa sem furo'
+        } else {
+            bebidaEscolhida.tampa = 'tampa com furo'
+        }
 
         console.log(`Você escolheu a entrega por ${entrega}`)
         bebidaEscolhida.entrega = entrega
 
-        console.log(`Seu pedido foi a bebida ${bebidaEscolhida.bebida}, no copo de ${bebidaEscolhida.copo}, com ${bebidaEscolhida.gelo} pedras de gelo, no copo de tamanho ${bebidaEscolhida.tamanho}, com a entrega sendo feita em ${bebidaEscolhida.entrega} `)
+        console.log(`Seu pedido foi a bebida ${bebidaEscolhida.bebida}, no copo de ${bebidaEscolhida.copo}, com ${bebidaEscolhida.gelo} pedras de gelo, no copo tamanho de ${bebidaEscolhida.tamanho}, com a entrega sendo feita em ${bebidaEscolhida.entrega}, com a ${bebidaEscolhida.tampa}. `)
+        rl.close()
     })
+}
+
+function pergunta(frase, escolha){
+    rl.question(frase, escolha)
 }
 
